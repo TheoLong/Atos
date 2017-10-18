830b74f04bee5e809bbf0b97bca0b8359ba91842
Xianze Meng <xianze@vt.edu>
2017-10-18 18:38:09 -0400

    added old version of state machine

--- firmware/src/Control.c
+++ firmware/src/Control.c
@@ -1,13 +1,10 @@
 #include "control.h"
 
-CONTROL_DATA controlData;
 QueueHandle_t receive_q;
 
 
 void CONTROL_Initialize ( void )
 {
-    /* Place the App state machine in its initial state. */
-    controlData.state = CONTROL_STATE_INIT;
     receive_q = xQueueCreate(128, sizeof(struct JsonResponse));
     DRV_ADC_Open();
     DRV_ADC_Start();
@@ -19,89 +16,7 @@ void CONTROL_Initialize ( void )
 
 void CONTROL_Tasks ( void )
 {
-    struct JsonResponse js = {0, 0, 0, 0, 0};
-    BaseType_t ret = xQueueReceive(receive_q, &js, (TickType_t) 5);
-    /* Check the application's current state. */
-    switch ( controlData.state )
-    {
-        /* Application's initial state. */
-        case CONTROL_STATE_INIT:
-        {
-            bool appInitialized = true;
-       
-        
-            if (appInitialized)
-            {
-            
-                controlData.state = CONTROL_STATE_RUN_TESTS;
-            }
-            break;
-        }
-        
-        case CONTROL_STATE_RUN_TESTS:
-        {
-            if(bumper >= 1)
-            {
-//                Left_Turn();
-//                Timing_Wait(500);
-//                while(!GetTimingFlag())
-//                {
-//                }
-//                Right_Turn();
-//                Timing_Wait(500);
-//                while(!GetTimingFlag())
-//                {
-//                }
-                //Move(45, 7000, FORWARD);
-                Left_Motor_PID(BACKWARD, 36);
-                Right_Motor_PID(BACKWARD, 35);
-//                Timing_Wait(20000);
-//                while(!GetTimingFlag())
-//                {
-//                }
-//                Left_Motor_PID(FORWARD, 0);
-//                Right_Motor_PID(FORWARD, 0);
-//                SetServo2PWM(770);
-//                Wait_Time(800);
-//                SetServo2PWM(0);
-//                Wait_Time(800);
-//                SetServo2PWM(50);
-//                Wait_Time(800);
-//                SetServo2PWM(0);
-//                Wait_Time(800);
-                controlData.state = CONTROL_STATE_SERVICE_TASKS;
-                break;
-            }
-            
-        }
-
-        case CONTROL_STATE_SERVICE_TASKS:
-        {
-            volatile int df;
-            volatile int ds;
-            if(ret != pdTRUE)
-            {
-                //TODO: dbgHLT
-            }
-            if(js.tsk == 49)
-            {
-                df = js.arg0;
-                ds = js.arg1;
-            }
-            if(js.tsk == 50)
-            {
-
-            }
-            //Wait_Time(800);
-//            struct JsonRequest js = {PIC_ID, 's',31, 0, 1, 0, 0, 0};
-//            SendOverWiFi(js);
-            break;
-        }
-        default:
-        {
-            break;
-        }
-    }
+ 
 }
 
 
--- firmware/src/Control.h
+++ firmware/src/Control.h
@@ -10,19 +10,6 @@
 #include "public.h"
 #include "timers.h"
 
-typedef enum
-{
-	CONTROL_STATE_INIT=0,
-    CONTROL_STATE_RUN_TESTS,
-	CONTROL_STATE_SERVICE_TASKS,
-    CONTROL_STATE_END,
-} CONTROL_STATES;
-
-
-typedef struct
-{
-    CONTROL_STATES state;
-} CONTROL_DATA;
 
 void CONTROL_Initialize ( void );
 void CONTROL_Tasks( void );
