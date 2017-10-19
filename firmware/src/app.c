15ed6d09ec7784629c8bd01290e21961eb5de5f7
Xianze Meng <xianze@vt.edu>
2017-10-18 21:45:06 -0400

    Factory

--- firmware/src/app.c
+++ firmware/src/app.c
@@ -6,6 +6,7 @@ TimerHandle_t mytimer0;
 TimerHandle_t ServoTimer1, ServoTimer2, ServoTimer3;
 QueueHandle_t ServoQueue;
 
+bool load = false;
 
 void APP_Initialize ( void )
 {
@@ -13,7 +14,7 @@ void APP_Initialize ( void )
     DRV_TMR0_Start();
     ServoTimer1 = xTimerCreate("ServoTimer1", 10, pdTRUE, (void*)0, ServoControlCallback1);
     ServoTimer2 = xTimerCreate("ServoTimer2", 200, pdTRUE, (void*)1, ServoControlCallback2);
-    ServoTimer3 = xTimerCreate("ServoTimer3", 250, pdTRUE, (void*)2, ServoControlCallback3);
+    ServoTimer3 = xTimerCreate("ServoTimer3", 500, pdTRUE, (void*)2, ServoControlCallback3);
     xTimerStart(ServoTimer1, (TickType_t)3);
     xTimerStart(ServoTimer3, (TickType_t)3);
     ServoQueue = xQueueCreate(128, sizeof(struct JsonResponse));
@@ -49,13 +50,17 @@ void APP_Tasks ( void )
         {
             if(jsr.tsk == 80 && jsr.arg0 == 1)
             {
+                struct JsonRequest jsr = {3, 's', 3, 80, 0, 0, 0, 0, 0};
+                SendOverWiFi(jsr);
+                load = true;
                 DRV_OC0_PulseWidthSet(800);
                 vTaskDelay((TickType_t)140);
                 xTimerStop(ServoTimer1, 5);
-                xTimerStop(ServoTimer3, 5);
-                xTimerStart(ServoTimer2, 5);
+//                xTimerStop(ServoTimer3, 5);
+                xTimerStart(ServoTimer2, 5);            
             }
         }
+
     }
 }
 
@@ -87,15 +92,19 @@ void ServoControlCallback2(TimerHandle_t xTimer)
 {
     static int pos = 50;
     static int counter = 0;
+    if(load)
+    {
+        load = false;
+        counter = 0;
+    }
     DRV_OC0_PulseWidthSet(pos);
     pos = (pos == 50) ? 850 : 50;
-    if(counter == 94)
+    if(counter >= 78)
     {
         struct JsonRequest jsr = {3, 's', 0, 80, 0, 0, 0, 0, 0};
         SendOverWiFi(jsr);
         xTimerStop(xTimer, (TickType_t)10);
         xTimerStart(ServoTimer1, (TickType_t)5);
-        xTimerStart(ServoTimer3, (TickType_t)5);
         counter = 0;
     }
     counter++;
